e09af022e35d7dfb33da06907c86d25601f5b7e2
offf <isvind23@gmail.com>
2015-11-11 23:12:25 +0100

    Quiz 4

---
+++ Quiz 4/Quiz 4.R
@@ -0,0 +1,17 @@
+#Q1
+set.seed(1)
+rpois(5, 2)
+
+#Q5
+set.seed(10)
+x <- rep(0:1, each = 5)
+e <- rnorm(10, 0, 20)
+y <- 0.5 + 2 * x + e
+
+plot(x,y)
+
+#Q8
+library(datasets)
+Rprof()
+fit <- lm(y ~ x1 + x2)
+Rprof(NULL)
